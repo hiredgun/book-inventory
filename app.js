bcbc1d2b1b855174611c77c3d39d6aa1e7e23d8a
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-03-16 15:01:15 +0100

    repository pattern

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var stockRepository = require('./stockRepository');
 
 function logIncoming(req, res, next) {
     console.log('1 log incoming');
@@ -19,41 +20,23 @@ app.get('/', auth, function (req, res, next) {
     res.send('Hello World!');
 });
 
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/book_inventory_store';
-var collection = null;
-
-var collectionPromise = MongoClient.
-    connect(url).
-    then(function (db) {
-        return db.collection('books');
-    });
 
 app.post('/stock', function (req, res, next) {
     var isbn = req.body.isbn;
     var count = req.body.count;
 
-    collectionPromise.
-        then(function (collection) {
-            //throw new Error('sth bad');
-            return collection.updateOne(
-                {isbn: isbn},
-                {isbn: isbn, count: count},
-                {upsert: true});
-        }).
+    stockRepository.
+        stockUp(isbn, count).
         then(function (result) {
             res.json({isbn: req.body.isbn, count: req.body.count});
         }).
         catch(next);
-
 });
 
 
 app.get('/stock', function (req, res, next) {
-    collectionPromise.
-        then(function (collection) {
-            return collection.find({}).toArray();
-        }).
+    stockRepository.
+        findAll().
         then(function (docs) {
             res.json(docs);
         }).
---
+++ stockRepository.js
@@ -0,0 +1,24 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/book_inventory_store';
+
+var collectionPromise = MongoClient.
+    connect(url).
+    then(function (db) {
+        return db.collection('books');
+    });
+
+module.exports = {
+    findAll: function() {
+        return collectionPromise.then(function(collection) {
+            return collection.find({}).toArray();
+        });
+    },
+    stockUp: function(isbn, count) {
+        return collectionPromise.then(function (collection) {
+            return collection.updateOne({isbn: isbn}, {
+                isbn: isbn,
+                count: count
+            }, {upsert: true});
+        });
+    }
+};
