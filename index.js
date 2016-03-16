931f3f75870d62ae628bcbb28ffc9d9796b3b113
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-03-16 11:39:37 +0100

    error handling

--- index.js
+++ index.js
@@ -17,9 +17,24 @@ app.get('/', auth, function (req, res, next) {
     res.send('Hello World!');
 });
 
-app.get('/stock', function(req, res, next) {
+app.get('/stock', function (req, res, next) {
+    throw new Error("database migration - fancy word for downtime");
     res.send('Stock');
 });
+
+app.use(clientError);
+app.use(serverError);
+
+function clientError(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+}
+function serverError(err, req, res, next) {
+    res.status(err.status || 500);
+    console.error(err.stack);
+    res.json({message: err.message, error: (process.env.NODE_ENV === 'production') ? {} : err.stack});
+}
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
